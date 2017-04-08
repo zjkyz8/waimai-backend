09113e3c7126e1493e7b8519c400dd85089f0417
Xin Huang <zjkyz8@163.com>
2017-04-08 09:42:20 +0800

    [xhuang] 添加地址

--- controller/address.js
+++ controller/address.js
@@ -13,9 +13,24 @@ router.get('/', function* () {
             message: '无效的Access Token.',
             status: 401
         };
-        return this.status = 201;
+        return this.status = 401;
     }
     this.body = db.get('addresses').filter({user_id: userId}).value();
+    this.status = 201;
+});
+
+router.post('/', function* () {
+    let userId = AuthService.mapTokenToUser(this.request.header.authorization);
+    if(!userId){
+        this.body = {
+            name: 'Unauthorized',
+            message: '无效的Access Token.',
+            status: 401
+        };
+        return this.status = 401;
+    }
+    this.request.body.user_id = userId;
+    this.body = db.get('addresses').insert(this.request.body).write();
     this.status = 201;
 });
 
